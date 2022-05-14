f8030542a111fb619fcb3cf073db3a27e71349d2
Leon Lu <ltlu1993@gmail.com>
2022-05-14 18:57:49 -0400

    Initial Commit

---
+++ HW4.5-Stack/src/Queue.java
@@ -0,0 +1,28 @@
+public class Queue<V> {
+
+    @SuppressWarnings("unchecked")
+    public Queue(int maxSize) {
+    }
+
+    public int getMaxSize() {
+    }
+
+    public int getCurrentSize() {
+    }
+
+    public boolean isEmpty() {
+    }
+
+    public boolean isFull() {
+    }
+
+    public V top() {
+    }
+
+    public void enqueue(V value) {
+    }
+
+    public V dequeue() {
+    }
+    
+}
